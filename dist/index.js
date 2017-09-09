abecf5c744de9b16a6c201b56eb28b44496f811a
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-09-09 17:25:38 +0200

    error handling, listening handling

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,51 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //------------------------------------------------------------------------------
 // Import all packages
 //------------------------------------------------------------------------------
+const http = require("http");
+const debug = require("debug");
 const app_1 = require("./app");
+//------------------------------------------------------------------------------
+// Debug function
+//------------------------------------------------------------------------------
+debug('ts-express:server');
+//------------------------------------------------------------------------------
+// Error function
+//------------------------------------------------------------------------------
+function error(error) {
+    if (error.syscall !== 'listen')
+        throw error;
+    const bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+//------------------------------------------------------------------------------
+// Listening function
+//------------------------------------------------------------------------------
+function listening() {
+    const addr = server.address();
+    const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+    debug(`Listening on ${bind}`);
+}
+//------------------------------------------------------------------------------
+// Setup port
+//------------------------------------------------------------------------------
+const port = process.env.PORT || 3000;
+app_1.default.set('port', port);
 //------------------------------------------------------------------------------
 // Start server
 //------------------------------------------------------------------------------
-const port = process.env.PORT || 3000;
-app_1.default.listen(port, (err) => {
+const server = http.createServer(app_1.default);
+server.listen(port, (err) => {
     // Error message
     if (err) {
         return console.log(err);
@@ -16,4 +55,6 @@ app_1.default.listen(port, (err) => {
     // Success server running
     return console.log(`Server running at https://127.0.0.1:${port}`);
 });
+server.on('error', error);
+server.on('listening', listening);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,60 @@
 //------------------------------------------------------------------------------
 // Import all packages
 //------------------------------------------------------------------------------
+import * as http from 'http';
+import * as debug from 'debug';
 import app from './app';
+
+
+//------------------------------------------------------------------------------
+// Debug function
+//------------------------------------------------------------------------------
+debug('ts-express:server');
+
+
+//------------------------------------------------------------------------------
+// Error handling
+//------------------------------------------------------------------------------
+function error(error: NodeJS.ErrnoException): void {
+  if (error.syscall !== 'listen') throw error;
+  const bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+//------------------------------------------------------------------------------
+// Listening handling
+//------------------------------------------------------------------------------
+function listening(): void {
+  const addr = server.address();
+  const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
+
+
+//------------------------------------------------------------------------------
+// Setup port
+//------------------------------------------------------------------------------
+const port = process.env.PORT || 3000;
+app.set('port', port);
 
 
 //------------------------------------------------------------------------------
 // Start server
 //------------------------------------------------------------------------------
-const port = process.env.PORT || 3000;
-
-app.listen(port, (err: any) => {
+const server = http.createServer(app);
+server.listen(port, (err: any) => {
   // Error message
   if (err) {
     return console.log(err);
@@ -18,3 +63,5 @@ app.listen(port, (err: any) => {
   // Success server running
   return console.log(`Server running at https://127.0.0.1:${port}`);
 });
+server.on('error', error);
+server.on('listening', listening);
